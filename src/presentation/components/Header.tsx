f906dd5b57ccbd243cbbb2463e72cc4ce51fb276
victor <victor-brambilla@hotmail.com>
2022-08-24 10:20:06 -0400

    feat: Creating header

---
+++ src/presentation/components/Header.tsx
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react'
+import { Image, StyleSheet } from 'react-native';
+import { Appbar } from 'react-native-paper'
+
+export const Header = () => {
+
+  return (
+    <>
+    <StatusBar style={'light'}/>
+    <Appbar.Header dark={true} style={styles.container}>
+    <Image source={require('../../../assets/logo.png')} style={styles.logo} />
+    </Appbar.Header>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent:'center',
+    height: 80,
+    backgroundColor: 'rgb(18, 18, 18);s',
+  },
+  title:{
+    color: 'white',
+  },
+  logo: {
+    width: 200,
+    height: 60,
+  }
+});
