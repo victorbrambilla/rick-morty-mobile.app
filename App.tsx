f75aff0053f188788159cee79868da980a0da8e3
victor <victor-brambilla@hotmail.com>
2022-08-26 16:48:40 -0400

    feat: Add navigation

--- App.tsx
+++ App.tsx
@@ -1,26 +1,31 @@
 import * as React from 'react'
-import { MD3LightTheme as DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
+import { Provider as PaperProvider } from 'react-native-paper'
 import { CharacterProvider } from './src/presentation/hooks/useCharacter'
 import { Home } from './src/presentation/pages/Home'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator, NativeStackHeaderProps } from '@react-navigation/native-stack'
+import { Character } from './src/presentation/pages/Character'
+import { Header } from './src/presentation/components/Header'
+import { theme } from './src/presentation/theme/theme'
 
-export const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  version: 3,
-
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#03C03C',
-    secondary: '#653496',
-    background: 'rgb(18, 18, 18);',
-  },
-}
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <PaperProvider theme={theme}>
       <CharacterProvider>
-        <Home />
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              header: (props: NativeStackHeaderProps) => <Header props={props} />,
+              animation: 'slide_from_right',
+              animationDuration: 0,
+            }}
+          >
+            <Stack.Screen name='Home' component={Home} />
+            <Stack.Screen name='Character' component={Character} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </CharacterProvider>
     </PaperProvider>
   )
