f63492470260f8d3e2c8970202882f85f33c0971
victor <victor-brambilla@hotmail.com>
2022-08-27 15:01:06 -0400

    build: Disable eslint no explicit any

--- src/presentation/components/Filter.tsx
+++ src/presentation/components/Filter.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
 import { TextInput, useTheme } from 'react-native-paper'
 import { PaperSelect } from 'react-native-paper-select'
